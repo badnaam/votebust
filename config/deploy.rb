b82e28becfb676ad7ade276a93cd49b7307c90ca
asit <asit@ubuntu.(none)>
2010-08-01 20:51:05 -0700

    automated check in

--- config/deploy.rb
+++ config/deploy.rb
@@ -10,6 +10,7 @@ default_run_options[:pty] = true
 # Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
 
 set :user, 'asit'
+set :group, 'www-data'
 set :ssh_options, { :forward_agent => true }
 
 role :web, "server"                          # Your HTTP server, Apache/etc
@@ -30,10 +31,12 @@ namespace :deploy do
    
     after "deploy:symlink" do
         chown_to_www_data
-#        run "sudo chmod -R 0666 #{current_path}/log"
-#        run "sudo chmod -R 755 #{current_path}/log"
     end
 
+    after "deploy:setup" do
+        create_shared_dirs
+    end
+    
     after "deploy:update_code" do
         symlink_shared
     end
@@ -50,14 +53,53 @@ namespace :deploy do
     end
 
     desc "Create shared links"
-    task :symlink_shared do
+    task :symlink_shared, :roles => :app  do
+        #Copy the files firest
+        upload("config/database.yml", "#{shared_path}/config", :via => :scp)
+        upload("config/sphinx.yml", "#{shared_path}/config", :via => :scp)
         run "ln -nfs #{shared_path}/config/database.yml #{release_path}/config/database.yml"
+        run "ln -nfs #{shared_path}/config/database.yml #{release_path}/config/sphinx.yml"
         run "ln -nfs #{shared_path}/assets #{release_path}/public/assets"
+    end
+
+    desc "Create additional shared directories"
+    task :create_shared_dirs do
+        run "mkdir -p #{shared_path}/assets/images/users"
+        run "mkdir #{shared_path}/config"
     end
 
     desc "Change group to www-data"
     task :chown_to_www_data, :roles => [ :app, :db, :web ] do
         sudo "chown -R #{user}:www-data #{deploy_to}"
         sudo "chmod -R 755 #{deploy_to}"
-    end 
+    end
+
+    task :install_log_rotate_script, :roles => :app do
+        rotate_script = %Q{#{shared_path}/log/#{rails_env}.log {
+        daily
+        rotate 14
+        size 5M
+        compress
+        create 640 #{user} #{group}
+        missingok
+        }}
+        put rotate_script, "#{shared_path}/logrotate_script"
+        sudo "cp #{shared_path}/logrotate_script /etc/logrotate.d/#{application}"
+        delete "#{shared_path}/logrotate_script"
+    end
+
+    set :rake_cmd, (ENV['RAKE_CMD'] || nil)
+
+    task :rake_exec do
+        if rake_cmd
+            run "cd #{current_path} && #{rake} #{rake_cmd} RAILS_ENV=#{rails_env}"
+        end
+    end
+
+    # Clear file-based fragment and/or page cache
+    task :clear_cache do
+        # I usually make a custom Rake task for this
+        set :rake_cmd, "tmp:cache:clear"
+        rake_exec
+    end
 end
