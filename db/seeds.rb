2e9fc7add33641014c1fff550b7054fa320cf226
asit <asit@ubuntu.(none)>
2010-08-01 18:55:12 -0700

    automated check in

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,5 +9,5 @@ if Role.count == 0
     Role.create(:name => 'admin')
     Role.create(:name => 'user')
 end
-User.create(:username => :admin, :password => 't', :password_confirmation => 't', :active => 1, :age => 34,
+User.create(:username => 'admin', :password => 't', :password_confirmation => 't', :active => 1, :age => 34,
     :zip => 94577, :sex => 1, :email => 'admin@votechek.com', :role_id => Role.find_by_name('admin').id)
