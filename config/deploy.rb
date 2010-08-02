473d3c094696ce6a1682363c27a237f941ab02bd
asit <asit@ubuntu.(none)>
2010-08-02 14:17:56 -0700

    automated check in

--- config/deploy.rb
+++ config/deploy.rb
@@ -36,19 +36,19 @@ namespace :deploy do
     end
 
     after "deploy:stop" do
-        #        delayed_job_stop
-        "dj:stop"
+                delayed_job_stop
+#        "dj:stop"
     end
     after "deploy:start" do
-        #        delayed_job_start
-        "dj:start"
+                delayed_job_start
+#        "dj:start"
     end
     after "deploy:restart" do
-        #        delayed_job_restart
-        "dj:restart"
+                delayed_job_restart
+#        "dj:restart"
     end
 
-    before "deploy:update_code", "dj:stop"
+#    before "deploy:update_code", "dj:stop"
     
     after "deploy:symlink" do
         chown_to_www_data
@@ -191,36 +191,3 @@ namespace :deploy do
    
 end
 
-namespace :dj do
-
-  desc "Start delayed_job daemon."
-  task :start, :roles => :app do
-    run "if [ -d #{current_path} ]; then cd #{current_path} && sudo RAILS_ENV=#{rails_env} script/delayed_job start; fi"
-  end
-
-  desc "Stop delayed_job daemon."
-  task :stop, :roles => :app do
-    run "if [ -d #{current_path} ]; then cd #{current_path} && sudo RAILS_ENV=#{rails_env} script/delayed_job stop; fi"
-  end
-
-  desc "Restart delayed_job daemon."
-  task :restart, :roles => :app do
-    run "if [ -d #{current_path} ]; then cd #{current_path} && sudo RAILS_ENV=#{rails_env} script/delayed_job restart; fi"
-  end
-
-  desc "Show delayed_job daemon status."
-  task :status, :roles => :app do
-    run "if [ -d #{current_path} ]; then cd #{current_path} && sudo RAILS_ENV=#{rails_env} script/delayed_job status; fi"
-  end
-
-  desc "List the PIDs of all running delayed_job daemons."
-  task :pids, :roles => :app do
-    run "sudo lsof | grep '#{deploy_to}/shared/log/delayed_job.log' | cut -c 1-21 | uniq | awk '/^ruby/ {if(NR > 0){system(\"echo \" $2)}}'"
-  end
-
-  desc "Kill all running delayed_job daemons."
-  task :kill, :roles => :app do
-    run "sudo lsof | grep '#{deploy_to}/shared/log/delayed_job.log' | cut -c 1-21 | uniq | awk '/^ruby/ {if(NR > 0){system(\"kill -9 \" $2)}}'"
-    run "if [-d #{current_path} ]; then cd #{current_path} && sudo RAILS_ENV=#{rails_env} script/delayed_job stop; fi" # removes orphaned pid file(s)
-  end
-end
