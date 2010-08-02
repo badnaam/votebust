72c44387619af1e8351fc32249a90024d66da64f
asit <asit@ubuntu.(none)>
2010-08-02 14:08:09 -0700

    automated check in

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,3 +1,5 @@
+require 'delayed_job.rb'
+
 set :application, "votechek"
 set :deploy_to, "/var/www/#{application}"
 set :scm, :git
@@ -186,38 +188,5 @@ namespace :deploy do
     task :delayed_job_restart, :roles => :app do
         run "cd #{current_path};#{get_rails_env} script/delayed_job restart"
     end
-
-    namespace :dj do
-
-        desc "Start delayed_job daemon."
-        task :start, :roles => :app do
-            run "if [ -d #{current_path} ]; then cd #{current_path} && sudo RAILS_ENV=#{rails_env} script/delayed_job start; fi"
-        end
-
-        desc "Stop delayed_job daemon."
-        task :stop, :roles => :app do
-            run "if [ -d #{current_path} ]; then cd #{current_path} && sudo RAILS_ENV=#{rails_env} script/delayed_job stop; fi"
-        end
-
-        desc "Restart delayed_job daemon."
-        task :restart, :roles => :app do
-            run "if [ -d #{current_path} ]; then cd #{current_path} && sudo RAILS_ENV=#{rails_env} script/delayed_job restart; fi"
-        end
-
-        desc "Show delayed_job daemon status."
-        task :status, :roles => :app do
-            run "if [ -d #{current_path} ]; then cd #{current_path} && sudo RAILS_ENV=#{rails_env} script/delayed_job status; fi"
-        end
-
-        desc "List the PIDs of all running delayed_job daemons."
-        task :pids, :roles => :app do
-            run "sudo lsof | grep '#{deploy_to}/shared/log/delayed_job.log' | cut -c 1-21 | uniq | awk '/^ruby/ {if(NR > 0){system(\"echo \" $2)}}'"
-        end
-
-        desc "Kill all running delayed_job daemons."
-        task :kill, :roles => :app do
-            run "sudo lsof | grep '#{deploy_to}/shared/log/delayed_job.log' | cut -c 1-21 | uniq | awk '/^ruby/ {if(NR > 0){system(\"kill -9 \" $2)}}'"
-            run "if [-d #{current_path} ]; then cd #{current_path} && sudo RAILS_ENV=#{rails_env} script/delayed_job stop; fi" # removes orphaned pid file(s)
-        end
-    end
+   
 end
