46ac297acaff4e959e2a7f2b27895e78be649e22
badnaam <asitkmishra@gmail.com>
2010-09-30 16:02:42 -0700

    automated check in

--- app/models/category.rb
+++ app/models/category.rb
@@ -7,6 +7,8 @@ class Category < ActiveRecord::Base
     has_friendly_id :name, :use_slug => true
     has_many :users, :through => :interests
     has_many :interests
+
+    after_create :refresh_category_cache
     
     default_scope :order => 'categories.name ASC'
 
@@ -18,19 +20,19 @@ class Category < ActiveRecord::Base
         self.new_record?
     end
 
-    def self.all_categories
-        Rails.cache.fetch("all_categories_#{list_key}") do
+    def self.filled_categories
+        Rails.cache.fetch("filled_categories_#{list_key}") do
             Category.all(:include => [:slug], :conditions => ['vote_topics_count > ?', 0])
+        end
+    end
+    
+    def self.all_categories
+        Rails.cache.fetch("all_categories") do
+            Category.all(:include => :slug)
         end
     end
 
     def self.list_key
         sum(:vote_topics_count)
-    end
-
-    def self.test
-        begin
-        rescue => exp
-        end
     end
 end
