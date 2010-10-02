d269577fa26c5d316b006dbb22aa927106e639b3
badnaam <asitkmishra@gmail.com>
2010-10-02 15:39:55 -0700

    automated check in

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,8 @@
 class UsersController < ApplicationController
     layout proc { |controller| ["show", "edit"].include?(controller.action_name) ? 'main' : 'login' }
     #   skip_before_filter :require_user, :only => [:top_voters]
-    skip_before_filter :require_registration, :only => [:edit, :update]
-    before_filter :require_no_user, :only => [:new, :create, :top_voters]
+    skip_before_filter :require_registration, :only => [:edit, :update, :top_voters]
+    before_filter :require_no_user, :only => [:new, :create]
     before_filter :require_user, :except => [:new, :create, :top_voters]
     before_filter :store_location, :only => [:show]
     filter_access_to [:edit, :update], :attribute_check => true
