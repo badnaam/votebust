13859dd3cc1315aa39e9faca3379cfab49e8b82f
badnaam <asitkmishra@gmail.com>
2010-10-02 12:48:19 -0700

    deleted migration

--- db/migrate/20100707072341_add_total_votes_to_vote_topics.rb
+++
@@ -1,9 +0,0 @@
-class AddTotalVotesToVoteTopics < ActiveRecord::Migration
-  def self.up
-    add_column :vote_topics, :total_votes, :integer
-  end
-
-  def self.down
-    remove_column :vote_topics, :total_votes
-  end
-end
